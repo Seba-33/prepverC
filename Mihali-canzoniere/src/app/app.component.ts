bcb2be6f053e547c5d5fdf6f6136ae779cba5548
Seba-33 a <mihali.sebastian@iisgalvanimi.edu.it>
2025-06-04 18:03:11 +0000

    punto 4

--- Mihali-canzoniere/src/app/app.component.ts
+++ Mihali-canzoniere/src/app/app.component.ts
@@ -4,18 +4,19 @@ import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Song } from './Models/song.model';
 import { CommonModule } from '@angular/common';
+import { SongListComponent } from './song-list/song-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet,CommonModule, SongListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'Mihali-canzoniere';
   loading! : boolean
-  obs! : Observable<object>
+  obs! : Observable<Song[]>
   data!: object
   http: HttpClient
   vettSong: Song [] = []
@@ -24,14 +25,14 @@ export class AppComponent implements OnInit{
   
   makeChiamta(){
     this.loading = true
-    this.obs = this.http.get('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
+    this.obs = this.http.get<Song[]>('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
     this.obs.subscribe(this.getData)
   }
 
-  getData = (d : object )=>{
-    this.data = d
+  getData = (d : Song[] )=>{
+    this.vettSong = d
     this.loading = false
-    console.log(this.data)
+    console.log(this.vettSong)
   }
 
   ngOnInit(): void {
--- Mihali-canzoniere/src/app/song-list/song-list.component.ts
+++ Mihali-canzoniere/src/app/song-list/song-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Song } from '../Models/song.model';
 
 @Component({
   selector: 'app-song-list',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './song-list.component.css'
 })
 export class SongListComponent {
-
+  @Input() canz! : Song
 }
